985e071e54daa4850ef814973a405ed62c4144ee
jc241001 <gerard.djian@my.jcu.edu.au>
2018-03-10 10:58:42 +1000

    Added name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,20 @@
 """Gerard Djian"""
+
+
+def main():  # Test skip_letter() function
+    for name in ["Harold", "Tom", "", "Constance", "Elizabeth", "Dictionary"]:
+        skip_letter(name)
+
+
+def skip_letter(persons_name):
+    # persons_name = input("Enter your name: ")
+    while persons_name == "":
+        print("Must enter something")
+        persons_name = input("Enter your name: ")
+
+    modified_persons_name = persons_name[::2]
+    print("Every second letter is: {}".format(modified_persons_name))
+
+
+if __name__ == '__main__':
+    main()
